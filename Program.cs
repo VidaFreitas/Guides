020a5ac9d58ba9e7e54879909abdfaa6c95f8f99
Vida Valessa <vidavalessa65@gmail.com>
2021-01-07 12:56:57 -0300

    Additional methods

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ namespace Guides
         static void Main(string[] args)
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.IndexOf("um"));
-            Console.WriteLine(texto.IndexOf("é"));
-            Console.WriteLine(texto.LastIndexOf("s"));
+            Console.WriteLine(texto.ToLower());
+            Console.WriteLine(texto.ToUpper());
+            Console.WriteLine(texto.Insert(5, "Aqui "));
+            Console.WriteLine(texto.Remove(5, 5));
         }
     }
 }
