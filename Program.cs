4399003e048577ebbbde678e159a4cf41d34cd6c
Vida Valessa <vidavalessa65@gmail.com>
2021-01-07 12:41:26 -0300

    Using Startswith/Endswith

--- Program.cs
+++ Program.cs
@@ -6,10 +6,14 @@ namespace Guides
     {
         static void Main(string[] args)
         {
-            var texto = "Esse texto é um teste";
-            Console.WriteLine(texto.Contains("Teste"));
-            Console.WriteLine(texto.Contains("teste"));
-            Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este"));
+            Console.WriteLine(texto.StartsWith("texto"));
+
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.EndsWith("Teste"));
+            Console.WriteLine(texto.EndsWith("texto"));
         }
     }
 }
