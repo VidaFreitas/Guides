72a95baefd505287819da4f951a7cdf61627639d
Vida Valessa <vidavalessa65@gmail.com>
2021-01-07 12:44:57 -0300

    Using equals

--- Program.cs
+++ Program.cs
@@ -7,13 +7,9 @@ namespace Guides
         static void Main(string[] args)
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.StartsWith("Este"));
-            Console.WriteLine(texto.StartsWith("este"));
-            Console.WriteLine(texto.StartsWith("texto"));
-
-            Console.WriteLine(texto.EndsWith("teste"));
-            Console.WriteLine(texto.EndsWith("Teste"));
-            Console.WriteLine(texto.EndsWith("texto"));
+            Console.WriteLine(texto.Equals("Este texto é um teste"));
+            Console.WriteLine(texto.Equals("O texto é um teste"));
+            Console.WriteLine(texto.Equals("Este texto é um Teste"));
         }
     }
 }
