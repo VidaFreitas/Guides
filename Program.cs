b9a0d5c4334334aeaceed6d3f59be751838d9c71
Vida Valessa <vidavalessa65@gmail.com>
2021-01-07 12:12:04 -0300

    Using Guids

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Guides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("5ff5b871-91db-47d4-9406-c45eb800776e");
+            Console.WriteLine(id.ToString().Substring(0, 8));
+        }
+    }
+}
